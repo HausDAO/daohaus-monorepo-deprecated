122eb3d678a6d7bae01ce929cf2b708eb52a5a35
jordan <jordan.lesich@gmail.com>
2022-04-15 10:24:17 -0700

    Create global font object

---
+++ libs/ui/src/styles/global/font.ts
@@ -0,0 +1,22 @@
+export const Font = {
+  Family: {
+    Body: `'Mulish', sans-serif`,
+    Data: `'Space Mono', monospace`,
+  },
+  Size: {
+    Xs: '1.2rem',
+    Sm: '1.4rem',
+    Md: '1.6rem',
+    Lg: '2rem',
+    Xl: '2.4rem',
+    '2xl': '3rem',
+    '3xl': '4rem',
+    '4xl': '4.8rem',
+  },
+  Weight: {
+    Reg: 400,
+    Med: 500,
+    Bold: 700,
+    Black: 900,
+  },
+};

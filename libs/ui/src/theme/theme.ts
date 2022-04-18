a41640ebcec98ed1a4a4487cf9c5fdb7391d8641
jordan <jordan.lesich@gmail.com>
2022-04-18 13:00:16 -0700

    remove tomato themes

--- libs/ui/src/theme/theme.ts
+++ libs/ui/src/theme/theme.ts
@@ -1,4 +1,4 @@
-import { sky, tomato, tomatoDark, skyDark } from '@radix-ui/colors';
+import { sky, skyDark } from '@radix-ui/colors';
 
 export const defaultDarkTheme = {
   themeName: 'dark',
@@ -11,14 +11,4 @@ export const defaultLightTheme: Theme = {
   themeName: 'light',
   BgColor: sky.sky1,
   FontColor: sky.sky12,
-};
-export const broodingTomatoDarkness: Theme = {
-  themeName: 'brood',
-  BgColor: tomatoDark.tomato2,
-  FontColor: tomatoDark.tomato10,
-};
-export const shimmeringTomatoLuminence: Theme = {
-  themeName: 'shimmer',
-  BgColor: tomato.tomato2,
-  FontColor: tomato.tomato10,
 };

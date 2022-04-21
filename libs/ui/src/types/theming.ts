99ef2bcfa8bafb46ac991237863af491b27b21da
Jord <53406838+jordanlesich@users.noreply.github.com>
2022-04-21 08:17:26 -0700

    Base input (#76)
    Add base input component

---
+++ libs/ui/src/types/theming.ts
@@ -0,0 +1,16 @@
+export type Theme = {
+  themeName: string;
+  bgColor: string;
+  fontColor: string;
+  primary: string;
+  error: string;
+  warning: string;
+  success: string;
+  info: string;
+  field: {
+    bg: string;
+    focus: string;
+    placeholderText: string;
+    disabled: string;
+  };
+};

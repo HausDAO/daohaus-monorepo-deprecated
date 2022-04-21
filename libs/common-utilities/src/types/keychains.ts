39335bce5319eaf912de9c880721358832ece172
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-04-21 09:14:18 -0600

    Feature/dao queries (#75)
    Add initial dao queries to the sdk

---
+++ libs/common-utilities/src/types/keychains.ts
@@ -0,0 +1,11 @@
+export type Keychain = {
+  '0x1'?: string;
+  '0x2a'?: string;
+  '0x4'?: string;
+  '0x64'?: string;
+  '0x89'?: string;
+  '0xa4b1'?: string;
+  '0xa4ec'?: string;
+};
+
+export type KeychainList = { [index: string]: Keychain };

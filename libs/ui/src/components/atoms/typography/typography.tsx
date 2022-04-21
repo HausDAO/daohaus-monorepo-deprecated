f3f1e1796a9915d0888738140e0830b2275b6ec9
jordan <jordan.lesich@gmail.com>
2022-04-21 16:50:32 -0700

    add color change utils to typography

--- libs/ui/src/components/atoms/typography/typography.tsx
+++ libs/ui/src/components/atoms/typography/typography.tsx
@@ -5,10 +5,12 @@ import styled from 'styled-components';
 const Par = styled.p`
   font-family: ${font.family.body};
   font-weight: ${font.weight.reg};
+  color: ${(props) => props.color};
 `;
 const Data = styled.p`
   font-family: ${font.family.data};
   font-weight: ${font.weight.reg};
+  color: ${(props) => props.color};
   letter-spacing: 1px;
 `;
 
@@ -32,31 +34,37 @@ export const H6 = styled.h6`
   font-size: ${font.size.md};
   font-weight: ${font.weight.black};
   font-family: ${font.family.body};
+  color: ${(props) => props.color};
 `;
 export const H5 = styled.h5`
   font-size: ${font.size.lg};
   font-weight: ${font.weight.bold};
   font-family: ${font.family.body};
+  color: ${(props) => props.color};
 `;
 export const H4 = styled.h4`
   font-size: ${font.size.xl};
   font-weight: ${font.weight.bold};
   font-family: ${font.family.body};
+  color: ${(props) => props.color};
 `;
 export const H3 = styled.h3`
   font-size: ${font.size.xxl};
   font-weight: ${font.weight.reg};
   font-family: ${font.family.body};
+  color: ${(props) => props.color};
 `;
 export const H2 = styled.h2`
   font-size: ${font.size.xxxl};
   font-weight: ${font.weight.light};
   font-family: ${font.family.body};
+  color: ${(props) => props.color};
 `;
 export const H1 = styled.h1`
   font-size: ${font.size.xxxxl};
   font-weight: ${font.weight.light};
   font-family: ${font.family.body};
+  color: ${(props) => props.color};
 `;
 ////////////////////DATA TEXT////////////////////
 export const DataXs = styled(Data)`

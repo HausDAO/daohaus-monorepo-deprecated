ff99a2992c77bed25bc069d267356bb275d2e43a
jordan <jordan.lesich@gmail.com>
2022-04-21 16:52:03 -0700

    update component folder structure as per the componetn folder structure agreement Henceforth!

---
+++ libs/ui/src/components/atoms/helperTexts/index.ts
@@ -0,0 +1,2 @@
+export * from './helperTexts';
+// export * from './helperTexts.stories';
--- libs/ui/src/components/atoms/input/inputType.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '../../../types/formAndField';
-import { IconType } from 'react-icons';
-
-export type InputType = Field & {
-  icon?: IconType;
-  long?: boolean;
-  full?: boolean;
-  number: boolean;
-};

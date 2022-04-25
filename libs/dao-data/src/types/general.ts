39a52b5a738dd18ca20912e883320db7c731ea5f
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-04-25 08:36:46 -0600

    Feature/query filters (#85)
    * gets basic queries working with new ts config rules
    
    * quick generic entity experiment
    
    * finishes scoped queries
    
    * gets basic queries working with new ts config rules
    
    * quick generic entity experiment
    
    * finishes scoped queries
    
    * initial cross chain query
    
    * adds list and find to query names
    
    * adds ordering, reworks queries to setup for code gen
    
    * fixes up some issues with entities and query syntax
    
    * address pr feedback

--- libs/dao-data/src/types/general.ts
+++ libs/dao-data/src/types/general.ts
@@ -1,6 +1,45 @@
-export type WhereQueryVariables = {
+import { Keychain } from '@daohaus/common-utilities';
+
+/**
+ * Query related types
+ */
+export interface ListQueryArguments {
+  networkId: keyof Keychain;
+  filter?: FilterPairs;
+  ordering?: Ordering;
+}
+
+export type FindQueryArguments = {
+  networkId: keyof Keychain;
+  dao: string;
+  memberAddress: string;
+  proposalId: string;
+};
+
+export interface GenericQueryArguments {
+  networkId: keyof Keychain;
+  query: string;
+  filter?: FilterPairs;
+}
+
+export interface CrossNetworkQueryArguments {
+  networks: string[];
+  account: string;
+}
+
+// rework this and filter pairs with code gen
+export type QueryVariables = {
   [field: string]: string;
 };
+
+export type FilterPairs = {
+  [field: string]: string;
+};
+
+export interface Ordering {
+  orderBy: string;
+  orderDirection: string;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface QueryResult<Data = any> {

3c63396765958260c04e6af8d0400503490eb353
jordan <jordan.lesich@gmail.com>
2022-04-26 16:45:39 -0700

    Revert "scaffold button, class generation"
    This reverts commit ac4f1ffd59f471553bf64206b7a13e327c6a3d4f.

--- libs/ui/src/components/atoms/button/button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Button from './button';
-
-export default {
-  title: 'Atoms/Button',
-  component: Button,
-} as ComponentMeta<typeof Button>;
-
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;

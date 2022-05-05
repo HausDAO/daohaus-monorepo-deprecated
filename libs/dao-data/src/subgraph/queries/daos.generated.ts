f6a166fe3604f5b837d537c4a1f50899456d240c
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-05-05 12:22:26 -0600

    Feature/account profile (#133)
    * generates the schema
    
    * adds some stuff of types generation
    
    * adds POC of graphql code gen
    
    * fixes path to schema types gen file
    
    * first query typed
    
    * res working with graphql request
    
    * wip filter query gen
    
    * adds gitignore
    
    * inits
    
    * Add ceramic
    
    * adds some ens fetch
    
    * closer on new query setup
    
    * adds all entity queries and generates types
    
    * refactors most queries into graphql with types
    
    * query and schema update
    
    * updates profile class to only return ens for now
    
    * fixes rpc urls for profile test
    
    * Add .env to build
    
    * more ignore
    
    * Fix
    
    * addresses feedback
    
    Co-authored-by: Keating <keating.dev@protonmail.com>

--- libs/dao-data/src/subgraph/queries/daos.generated.ts
+++ libs/dao-data/src/subgraph/queries/daos.generated.ts
@@ -9,16 +9,351 @@ export type ListDaosQueryVariables = Types.Exact<{
   orderDirection?: Types.OrderDirection;
 }>;
 
-
-export type ListDaosQuery = { daos: Array<{ id: string, createdAt: string, transactionHashSummon: string, lootAddress: string, safeAddress: string, lootPaused: boolean, sharesPaused: boolean, gracePeriod: string, votingPeriod: string, proposalOffering: string, quorumPercent: string, sponsorThreshold: string, minRetentionPercent: string, shareTokenName?: string | undefined, shareTokenSymbol?: string | undefined, totalShares: string, totalLoot: string, latestSponsoredProposalId: string, metaData?: { name?: string | undefined } | undefined }> };
+export type ListDaosQuery = {
+  daos: Array<{
+    id: string;
+    createdAt: string;
+    transactionHashSummon: string;
+    lootAddress: string;
+    safeAddress: string;
+    lootPaused: boolean;
+    sharesPaused: boolean;
+    gracePeriod: string;
+    votingPeriod: string;
+    proposalOffering: string;
+    quorumPercent: string;
+    sponsorThreshold: string;
+    minRetentionPercent: string;
+    shareTokenName?: string | undefined;
+    shareTokenSymbol?: string | undefined;
+    totalShares: string;
+    totalLoot: string;
+    latestSponsoredProposalId: string;
+    metaData?: { name?: string | undefined } | undefined;
+  }>;
+};
 
 export type FindDaoQueryVariables = Types.Exact<{
   id: Types.Scalars['ID'];
 }>;
 
+export type FindDaoQuery = {
+  dao?:
+    | {
+        id: string;
+        createdAt: string;
+        transactionHashSummon: string;
+        lootAddress: string;
+        safeAddress: string;
+        lootPaused: boolean;
+        sharesPaused: boolean;
+        gracePeriod: string;
+        votingPeriod: string;
+        proposalOffering: string;
+        quorumPercent: string;
+        sponsorThreshold: string;
+        minRetentionPercent: string;
+        shareTokenName?: string | undefined;
+        shareTokenSymbol?: string | undefined;
+        totalShares: string;
+        totalLoot: string;
+        latestSponsoredProposalId: string;
+        metaData?: { name?: string | undefined } | undefined;
+      }
+    | undefined;
+};
 
-export type FindDaoQuery = { dao?: { id: string, createdAt: string, transactionHashSummon: string, lootAddress: string, safeAddress: string, lootPaused: boolean, sharesPaused: boolean, gracePeriod: string, votingPeriod: string, proposalOffering: string, quorumPercent: string, sponsorThreshold: string, minRetentionPercent: string, shareTokenName?: string | undefined, shareTokenSymbol?: string | undefined, totalShares: string, totalLoot: string, latestSponsoredProposalId: string, metaData?: { name?: string | undefined } | undefined } | undefined };
-
-
-export const ListDaosDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"listDaos"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"where"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Dao_filter"}}},"defaultValue":{"kind":"ObjectValue","fields":[]}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"skip"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}},"defaultValue":{"kind":"IntValue","value":"0"}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"first"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}},"defaultValue":{"kind":"IntValue","value":"100"}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"orderBy"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Dao_orderBy"}}},"defaultValue":{"kind":"EnumValue","value":"id"}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"orderDirection"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"OrderDirection"}}},"defaultValue":{"kind":"EnumValue","value":"asc"}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"daos"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"where"},"value":{"kind":"Variable","name":{"kind":"Name","value":"where"}}},{"kind":"Argument","name":{"kind":"Name","value":"skip"},"value":{"kind":"Variable","name":{"kind":"Name","value":"skip"}}},{"kind":"Argument","name":{"kind":"Name","value":"first"},"value":{"kind":"Variable","name":{"kind":"Name","value":"first"}}},{"kind":"Argument","name":{"kind":"Name","value":"orderBy"},"value":{"kind":"Variable","name":{"kind":"Name","value":"orderBy"}}},{"kind":"Argument","name":{"kind":"Name","value":"orderDirection"},"value":{"kind":"Variable","name":{"kind":"Name","value":"orderDirection"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"transactionHashSummon"}},{"kind":"Field","name":{"kind":"Name","value":"lootAddress"}},{"kind":"Field","name":{"kind":"Name","value":"safeAddress"}},{"kind":"Field","name":{"kind":"Name","value":"lootPaused"}},{"kind":"Field","name":{"kind":"Name","value":"sharesPaused"}},{"kind":"Field","name":{"kind":"Name","value":"gracePeriod"}},{"kind":"Field","name":{"kind":"Name","value":"votingPeriod"}},{"kind":"Field","name":{"kind":"Name","value":"proposalOffering"}},{"kind":"Field","name":{"kind":"Name","value":"quorumPercent"}},{"kind":"Field","name":{"kind":"Name","value":"sponsorThreshold"}},{"kind":"Field","name":{"kind":"Name","value":"minRetentionPercent"}},{"kind":"Field","name":{"kind":"Name","value":"shareTokenName"}},{"kind":"Field","name":{"kind":"Name","value":"shareTokenSymbol"}},{"kind":"Field","name":{"kind":"Name","value":"totalShares"}},{"kind":"Field","name":{"kind":"Name","value":"totalLoot"}},{"kind":"Field","name":{"kind":"Name","value":"latestSponsoredProposalId"}},{"kind":"Field","name":{"kind":"Name","value":"metaData"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]} as unknown as DocumentNode<ListDaosQuery, ListDaosQueryVariables>;
-export const FindDaoDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"findDao"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"id"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"dao"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"id"},"value":{"kind":"Variable","name":{"kind":"Name","value":"id"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"transactionHashSummon"}},{"kind":"Field","name":{"kind":"Name","value":"lootAddress"}},{"kind":"Field","name":{"kind":"Name","value":"safeAddress"}},{"kind":"Field","name":{"kind":"Name","value":"lootPaused"}},{"kind":"Field","name":{"kind":"Name","value":"sharesPaused"}},{"kind":"Field","name":{"kind":"Name","value":"gracePeriod"}},{"kind":"Field","name":{"kind":"Name","value":"votingPeriod"}},{"kind":"Field","name":{"kind":"Name","value":"proposalOffering"}},{"kind":"Field","name":{"kind":"Name","value":"quorumPercent"}},{"kind":"Field","name":{"kind":"Name","value":"sponsorThreshold"}},{"kind":"Field","name":{"kind":"Name","value":"minRetentionPercent"}},{"kind":"Field","name":{"kind":"Name","value":"shareTokenName"}},{"kind":"Field","name":{"kind":"Name","value":"shareTokenSymbol"}},{"kind":"Field","name":{"kind":"Name","value":"totalShares"}},{"kind":"Field","name":{"kind":"Name","value":"totalLoot"}},{"kind":"Field","name":{"kind":"Name","value":"latestSponsoredProposalId"}},{"kind":"Field","name":{"kind":"Name","value":"metaData"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]} as unknown as DocumentNode<FindDaoQuery, FindDaoQueryVariables>;
+export const ListDaosDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'listDaos' },
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'where' },
+          },
+          type: {
+            kind: 'NonNullType',
+            type: {
+              kind: 'NamedType',
+              name: { kind: 'Name', value: 'Dao_filter' },
+            },
+          },
+          defaultValue: { kind: 'ObjectValue', fields: [] },
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'skip' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'Int' } },
+          },
+          defaultValue: { kind: 'IntValue', value: '0' },
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'first' },
+          },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'Int' } },
+          },
+          defaultValue: { kind: 'IntValue', value: '100' },
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'orderBy' },
+          },
+          type: {
+            kind: 'NonNullType',
+            type: {
+              kind: 'NamedType',
+              name: { kind: 'Name', value: 'Dao_orderBy' },
+            },
+          },
+          defaultValue: { kind: 'EnumValue', value: 'id' },
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: {
+            kind: 'Variable',
+            name: { kind: 'Name', value: 'orderDirection' },
+          },
+          type: {
+            kind: 'NonNullType',
+            type: {
+              kind: 'NamedType',
+              name: { kind: 'Name', value: 'OrderDirection' },
+            },
+          },
+          defaultValue: { kind: 'EnumValue', value: 'asc' },
+        },
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'daos' },
+            arguments: [
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'where' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'where' },
+                },
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'skip' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'skip' },
+                },
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'first' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'first' },
+                },
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'orderBy' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'orderBy' },
+                },
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'orderDirection' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'orderDirection' },
+                },
+              },
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'createdAt' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'transactionHashSummon' },
+                },
+                { kind: 'Field', name: { kind: 'Name', value: 'lootAddress' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'safeAddress' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'lootPaused' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'sharesPaused' },
+                },
+                { kind: 'Field', name: { kind: 'Name', value: 'gracePeriod' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'votingPeriod' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'proposalOffering' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'quorumPercent' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'sponsorThreshold' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'minRetentionPercent' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'shareTokenName' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'shareTokenSymbol' },
+                },
+                { kind: 'Field', name: { kind: 'Name', value: 'totalShares' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'totalLoot' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'latestSponsoredProposalId' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'metaData' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<ListDaosQuery, ListDaosQueryVariables>;
+export const FindDaoDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'findDao' },
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'id' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'ID' } },
+          },
+        },
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'dao' },
+            arguments: [
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'id' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'id' },
+                },
+              },
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'createdAt' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'transactionHashSummon' },
+                },
+                { kind: 'Field', name: { kind: 'Name', value: 'lootAddress' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'safeAddress' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'lootPaused' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'sharesPaused' },
+                },
+                { kind: 'Field', name: { kind: 'Name', value: 'gracePeriod' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'votingPeriod' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'proposalOffering' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'quorumPercent' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'sponsorThreshold' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'minRetentionPercent' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'shareTokenName' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'shareTokenSymbol' },
+                },
+                { kind: 'Field', name: { kind: 'Name', value: 'totalShares' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'totalLoot' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'latestSponsoredProposalId' },
+                },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'metaData' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<FindDaoQuery, FindDaoQueryVariables>;
--- libs/dao-data/test/queries.spec.ts
+++ libs/dao-data/test/queries.spec.ts
@@ -2,7 +2,7 @@ import { Haus } from '../src/index';
 
 describe('haus', () => {
   const rpcConfig = {
-    '0x4': 'https://<somekey>.rinkeby.rpc.rivet.cloud',
+    '0x4': `https://${process.env['RIVET_KEY']}.rinkeby.rpc.rivet.cloud`,
     '0x64': 'https://rpc.gnosischain.com',
   };
   let haus: Haus;
@@ -17,7 +17,7 @@ describe('haus', () => {
     const res = await haus.query.listDaos({
       networkId,
       ordering: {
-        orderBy: 'totalShares',
+        orderBy: 'createdAt',
         orderDirection: 'asc',
       },
     });

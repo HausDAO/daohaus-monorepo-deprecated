f1b8a5ec8f0499ddd8265d4ab521533ebd149db9
Alexander Keating <keating.dev@protonmail.com>
2022-05-11 10:17:46 -0400

    Fix jest issue (#146)
    Add ceramic profile

--- libs/dao-data/src/Profile.ts
+++ libs/dao-data/src/Profile.ts
@@ -1,6 +1,16 @@
+import { Core } from '@self.id/core';
+import { Caip10Link } from '@ceramicnetwork/stream-caip10-link';
 import { ethers } from 'ethers';
 
 import { Keychain } from '@daohaus/common-utilities';
+
+type BasicProfile = {
+  image?: string;
+  name?: string;
+  description?: string;
+  emoji?: string;
+  background?: string;
+};
 
 type AccountProfile = {
   address: string;
@@ -12,20 +22,41 @@ type AccountProfile = {
   background?: string;
 };
 
+const CERAMIC_NETWORK = process.env['CERAMIC_NETWORK'] || '';
+
 export default class Profile {
+  client: Core;
   providers: Keychain;
 
   constructor(providers: Keychain) {
     this.providers = providers;
+    this.client = new Core({ ceramic: CERAMIC_NETWORK || 'testnet-clay' });
   }
 
   public async get(address: string): Promise<AccountProfile> {
     const ens = await this.getEns(address);
-
+    const basicProfile = await this.getBasicProfile('0x1', address);
     return {
       address,
       ens,
+      image: basicProfile?.image,
+      name: basicProfile?.name,
+      description: basicProfile?.description,
+      emoji: basicProfile?.emoji,
+      background: basicProfile?.background,
     };
+  }
+
+  private async getBasicProfile(
+    chain: keyof Keychain = '0x1',
+    address: string
+  ): Promise<BasicProfile | null> {
+    const link = await Caip10Link.fromAccount(
+      this.client.ceramic,
+      `${address.toLowerCase()}@eip155:${Number(chain)}`
+    );
+
+    return this.client.get('basicProfile', link.did || '');
   }
 
   private async getEns(address: string): Promise<string | null> {

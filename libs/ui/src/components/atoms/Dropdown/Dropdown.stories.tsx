c3376b93cbe5d9b891206bdfd4773f54735cbe96
Rowdy <mroddy5280@gmail.com>
2022-05-19 14:19:43 -0600

    Feature/pascalcase names (#178)
    * Rename Button
    
    * Renamed Dropdown
    
    * HelperText
    
    * Input
    
    * InputLabel
    
    * Typography
    
    * Tooltip
    
    * Field Wrapper
    
    * Wrapped Input

---
+++ libs/ui/src/components/atoms/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,51 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { BiError } from 'react-icons/bi';
+import { Button } from '../Button/Button';
+import { ParXs } from '../Typography';
+import { DropdownItem, Dropdown } from './Dropdown';
+
+export default {
+  title: 'Atoms/Dropdown',
+  component: Dropdown,
+} as ComponentMeta<typeof Dropdown>;
+
+const Template: ComponentStory<typeof Dropdown> = (args) => (
+  <div style={{ display: 'flex', width: '100%', justifyContent: 'flex-end' }}>
+    <Dropdown {...args} />
+  </div>
+);
+const networkPanels = [
+  'Mainnet',
+  'Arbitrum',
+  'Celo',
+  'Gnosis',
+  'Optimism',
+  'Gnosis',
+  'Polygon',
+  'Kovan',
+  'Rinkeby',
+].map((item) => ({
+  type: 'clickable',
+  content: (
+    <Button secondary fullWidth leftAlign>
+      {item}
+    </Button>
+  ),
+})) as DropdownItem[];
+
+export const FullDropdown = Template.bind({});
+
+FullDropdown.args = {
+  spacing: '0.7rem',
+  width: '26rem',
+  align: 'end',
+  trigger: (
+    <Button IconLeft={BiError} tertiary>
+      Network Unavailable
+    </Button>
+  ),
+  items: [
+    { type: 'label', content: <ParXs>Switch to available network</ParXs> },
+    ...networkPanels,
+  ],
+};
--- libs/ui/src/components/atoms/helperTexts/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './helperTexts';
-export * from './helperTexts.stories';

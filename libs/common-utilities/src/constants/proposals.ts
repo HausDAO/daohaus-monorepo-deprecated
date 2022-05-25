9bd660c60d19ea77777a678cab67932a59467487
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-05-25 09:46:10 -0600

    Feature/data transformers (#190)
    * more ignore
    
    * wip
    
    * adds transformers
    
    * adds proposal status to list query
    
    * moves the proposal status helper to the dao-data lib to get rid of circular dependency
    
    * fixes warning
    
    * backout subgraph
    
    * addresses feedback

---
+++ libs/common-utilities/src/constants/proposals.ts
@@ -0,0 +1,24 @@
+export type ProposalStatus =
+  | 'Unsponsored'
+  | 'Voting'
+  | 'Grace'
+  | 'Expired'
+  | 'Cancelled'
+  | 'Ready for Processing'
+  | 'Failed'
+  | 'Passed'
+  | 'Processing Failed'
+  | 'Unknown';
+
+export const PROPOSAL_STATUS: { [index: string]: ProposalStatus } = {
+  unsponsored: 'Unsponsored',
+  voting: 'Voting',
+  grace: 'Grace',
+  expired: 'Expired',
+  cancelled: 'Cancelled',
+  needsProcessing: 'Ready for Processing',
+  failed: 'Failed',
+  passed: 'Passed',
+  actionFailed: 'Processing Failed',
+  unknown: 'Unknown',
+};

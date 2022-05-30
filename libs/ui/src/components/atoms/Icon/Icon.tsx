b2f702e073ce927d8d1603edeca09ca995bbded0
Rowdy <mroddy5280@gmail.com>
2022-05-29 21:42:03 -0600

    Refined Icon

--- libs/ui/src/components/atoms/Icon/Icon.tsx
+++ libs/ui/src/components/atoms/Icon/Icon.tsx
@@ -1,14 +1,9 @@
 import * as AccessibleIcon from '@radix-ui/react-accessible-icon';
 
 type IconProps = {
-  label: string;
-  className?: string;
+  label?: string;
 };
 
-export const Icon: React.FC<IconProps> = (props) => {
-  return (
-    <AccessibleIcon.Root label={props.label}>
-      {props.children}
-    </AccessibleIcon.Root>
-  );
+export const Icon: React.FC<IconProps> = ({ label = '', children }) => {
+  return <AccessibleIcon.Root label={label}>{children}</AccessibleIcon.Root>;
 };

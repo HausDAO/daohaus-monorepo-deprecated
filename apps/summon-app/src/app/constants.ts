80022c4eca08397a5714ecf60a1deed483dd6dc9
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-06-15 14:58:01 -0500

    Feature/app deployment testing (#270)
    * more ignore
    
    * tiny changes to summon and hub

--- apps/summon-app/src/app/constants.ts
+++ apps/summon-app/src/app/constants.ts
@@ -8,14 +8,6 @@ export const limitedNetworkTest = {
     rpc: `https://${import.meta.env.VITE_RIVET_KEY}.eth.rpc.rivet.cloud/`,
     explorer: 'https://etherscan.io',
   },
-  '0x4': {
-    chainId: '0x4',
-    networkId: 4,
-    name: 'Rinkeby',
-    symbol: 'ETH',
-    rpc: `https://${import.meta.env.VITE_RIVET_KEY}.rinkeby.rpc.rivet.cloud/`,
-    explorer: 'https://rinkeby.etherscan.io',
-  },
   '0x5': {
     chainId: '0x5',
     networkId: 5,

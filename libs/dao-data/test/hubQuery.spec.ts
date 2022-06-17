1cdc5bd27475e9a6b830374de7075388effd6205
jordan <jordan.lesich@gmail.com>
2022-06-17 15:09:35 -0700

    dderp

--- libs/dao-data/test/hubQuery.spec.ts
+++ libs/dao-data/test/hubQuery.spec.ts
@@ -1,4 +1,4 @@
-// import { Keychain } from '@daohaus/common-utilities';
+import { Keychain } from '@daohaus/common-utilities';
 import { Haus } from '../src/index';
 
 describe('haus', () => {
@@ -11,31 +11,30 @@ describe('haus', () => {
   beforeAll(async () => {
     haus = await Haus.create(rpcConfig);
   });
+
+  it('can fetch all daos for an account', async () => {
+    const networkIds: (keyof Keychain)[] = ['0x5'];
+    const memberAddress =
+      '0xf100041473280b594d78ab5fa4c44ba81edd367b'.toLowerCase();
+
+    const res = await haus.query.listDaosByMember({
+      memberAddress,
+      networkIds,
+    });
+
+    expect(res?.data?.daos.length).toBeGreaterThan(1);
+  });
+
+  it('can request tokens with the daos', async () => {
+    const networkIds: (keyof Keychain)[] = ['0x5'];
+    const memberAddress = '0xf100041473280b594d78ab5fa4c44ba81edd367b';
+
+    const res = await haus.query.listDaosByMember({
+      memberAddress,
+      networkIds,
+      includeTokens: true,
+    });
+
+    expect(res?.data?.daos.length).toBeGreaterThan(1);
+  });
 });
-
-// it('can fetch all daos for an account', async () => {
-//   const networkIds: (keyof Keychain)[] = ['0x5'];
-//   const memberAddress =
-//     '0xf100041473280b594d78ab5fa4c44ba81edd367b'.toLowerCase();
-
-//   const res = await haus.query.listDaosByMember({
-//     memberAddress,
-//     networkIds,
-//   });
-
-//   expect(res?.data?.daos.length).toBeGreaterThan(1);
-// });
-
-//   it('can request tokens with the daos', async () => {
-//     const networkIds: (keyof Keychain)[] = ['0x5'];
-//     const memberAddress = '0xf100041473280b594d78ab5fa4c44ba81edd367b';
-
-//     const res = await haus.query.listDaosByMember({
-//       memberAddress,
-//       networkIds,
-//       includeTokens: true,
-//     });
-
-//     expect(res?.data?.daos.length).toBeGreaterThan(1);
-//   });
-// })

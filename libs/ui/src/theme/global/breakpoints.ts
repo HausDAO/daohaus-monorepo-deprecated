c7d291ca3de8b9490fe2f08b0e39c8e04713e27b
jordan <jordan.lesich@gmail.com>
2022-06-18 13:39:01 -0700

    add breakpoints

---
+++ libs/ui/src/theme/global/breakpoints.ts
@@ -0,0 +1,12 @@
+export const breakpoints = {
+  xs: '480px',
+  sm: '768px',
+  md: '1024px',
+  lg: '1250px',
+};
+export const widthQuery = {
+  xs: `(max-width: ${breakpoints.xs}px)`,
+  sm: `(max-width: ${breakpoints.sm}px)`,
+  md: `(max-width: ${breakpoints.md}px)`,
+  lg: `(max-width: ${breakpoints.lg}px)`,
+};

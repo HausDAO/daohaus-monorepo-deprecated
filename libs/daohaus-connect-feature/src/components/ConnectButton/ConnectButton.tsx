043b545ca2741a4d4cef4370e37885fe3c3ee07f
Jord <53406838+jordanlesich@users.noreply.github.com>
2022-06-21 09:32:11 -0700

    Connect/mobile (#281)
    * Summoner form submits RHF data
    
    * update deps
    
    * refactor/review members segment
    
    * new pattern to handle onBlur.
    
    * refactor shaman and members patterns
    
    * build timeInput, wire in Timing section
    
    * wire in title and stake sections
    
    * clean
    
    * clean again
    
    * merge again
    
    * come on
    
    * add breakpoints
    
    * build media query hook
    
    * make it all responsive
    
    * fix breaking hub issue
    
    * Update libs/daohaus-connect-feature/src/components/NetworkButton/NetworkButton.tsx
    
    Update Pr 281 based on Rowdy's feedback
    
    Co-authored-by: Rowdy <mroddy5280@gmail.com>
    
    Co-authored-by: Jonathan Prozzi <jonathanprozzi@gmail.com>
    Co-authored-by: Rowdy <mroddy5280@gmail.com>

--- libs/daohaus-connect-feature/src/components/ConnectButton/ConnectButton.tsx
+++ libs/daohaus-connect-feature/src/components/ConnectButton/ConnectButton.tsx
@@ -1,41 +1,50 @@
 import { amberDark } from '@radix-ui/colors';
-import { RiUserAddLine } from 'react-icons/ri';
 import { Button, Spinner } from '@daohaus/ui';
+import { RiUserAddLine } from 'react-icons/ri';
+import classNames from 'classnames';
 
 import { useHausConnect } from '../../HausConnectContext';
 import { ButtonContainer } from './ConnectButtonStyles';
 import { UserConnectedDropdown } from './UserConnetedDropdown';
 
-export const ConnectButton = () => {
+export const ConnectButton = ({ isSm }: { isSm: boolean }) => {
   const { isConnected, isProfileLoading } = useHausConnect();
+
   if (!isConnected) {
-    return <ConnectWalletButton />;
+    return <ConnectWalletButton isSm={isSm} />;
   }
   if (isProfileLoading) {
-    return <LoadingButton />;
+    return <LoadingButton isSm={isSm} />;
   }
-  return <UserConnectedDropdown />;
+  return <UserConnectedDropdown isSm={isSm} />;
 };
 
-const ConnectWalletButton = () => {
+const ConnectWalletButton = ({ isSm }: { isSm: boolean }) => {
   const { connectWallet } = useHausConnect();
+  const classes = classNames({ 'mobile-connect-btn': isSm });
   return (
     <ButtonContainer>
-      <Button fullWidth IconLeft={RiUserAddLine} onClick={connectWallet}>
+      <Button
+        fullWidth={!isSm}
+        IconLeft={RiUserAddLine}
+        onClick={connectWallet}
+        sm={isSm}
+        className={classes}
+      >
         Connect Wallet
       </Button>
     </ButtonContainer>
   );
 };
 
-const LoadingButton = () => {
+const LoadingButton = ({ isSm }: { isSm: boolean }) => {
   return (
     <ButtonContainer>
-      <Button fullWidth>
+      <Button fullWidth={!isSm} sm={isSm}>
         <Spinner
           topColor={amberDark.amber8}
           bottomColor={amberDark.amber11}
-          size="2.8rem"
+          size={isSm ? '2rem' : '2.8rem'}
           strokeWidth=".3rem"
         />
       </Button>

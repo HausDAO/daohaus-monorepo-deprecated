6c76c5240d746c8e4d3b3bdee01e16192d2d642a
jordan <jordan.lesich@gmail.com>
2022-06-28 22:04:30 -0700

    centers column for mobile

--- apps/hub-app/src/components/DaoCards.tsx
+++ apps/hub-app/src/components/DaoCards.tsx
@@ -1,3 +1,4 @@
+import { widthQuery } from '@daohaus/ui';
 import styled from 'styled-components';
 import { TemporaryDAOType } from '../utils/appSpecificTypes';
 import { DaoCard } from './DaoCard';
@@ -11,7 +12,8 @@ const CardListBox = styled.div`
   flex-wrap: wrap;
   column-gap: 6rem;
   row-gap: 3rem;
-  .dao-card {
+  @media ${widthQuery.sm} {
+    justify-content: center;
   }
 `;
 export const DaoCards = ({ daoData }: CardsProps) => {

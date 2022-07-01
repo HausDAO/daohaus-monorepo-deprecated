2b1cd526ef37910600e2be1b8abd126222be0e85
Jord <53406838+jordanlesich@users.noreply.github.com>
2022-07-01 11:45:50 -0700

    Hub/filter query (#343)
    * revise types
    
    * test hub with summon
    
    * get ready for WIP push
    
    * hoist state and filter actions to page-level
    
    * build state and toggle for delegate
    
    * cheat toggle switchview over
    
    * fix dropdown alignment
    
    * refactor filter dropdown
    
    * query filter menu
    
    * delegate query
    
    * given queries do not work, skipping for now
    
    * switch to constants pattern
    
    * switch to const pattern
    
    * clean

--- apps/hub-app/src/components/TableControl.tsx
+++ apps/hub-app/src/components/TableControl.tsx
@@ -1,25 +1,12 @@
-import { Button, Dropdown, DropdownItem, Input, Theme } from '@daohaus/ui';
-import styled, { useTheme } from 'styled-components';
+import { Button, Input } from '@daohaus/ui';
+import styled from 'styled-components';
 import { BiSearch } from 'react-icons/bi';
-import { RiFilterFill } from 'react-icons/ri';
 import { BsFillGrid3X3GapFill } from 'react-icons/bs';
-import { AiOutlineCheck } from 'react-icons/ai';
 
-import { ParSm } from '@daohaus/ui';
 import { indigoDark } from '@radix-ui/colors';
 import { ListType } from '../utils/appSpecificTypes';
-import { MouseEvent, useState } from 'react';
-import { isValidNetwork, networkData } from '@daohaus/common-utilities';
-
-const IconFilter = styled(RiFilterFill)`
-  height: 1.8rem;
-  width: 1.8rem;
-  display: flex;
-  fill: ${indigoDark.indigo10};
-  :hover {
-    fill: ${indigoDark.indigo10};
-  }
-`;
+import { MouseEvent } from 'react';
+import FilterDropdown from './FilterDropdown';
 
 const IconGrid = styled(BsFillGrid3X3GapFill)`
   height: 1.8rem;
@@ -44,6 +31,10 @@ const Layout = styled.div`
   gap: 2.4rem;
   padding-top: 3.6rem;
   padding-bottom: 3.6rem;
+
+  .list-type-toggle {
+    transform: translateX(-13.5rem);
+  }
 `;
 
 const StyledInput = styled(Input)`
@@ -61,9 +52,20 @@ const StyledInput = styled(Input)`
 type TableControlProps = {
   listType: ListType;
   toggleListType: () => void;
+  filterNetworks: Record<string, string>;
+  toggleNetworkFilter: (event: MouseEvent<HTMLButtonElement>) => void;
+  filterDelegate: string;
+  toggleDelegateFilter: (event: MouseEvent<HTMLButtonElement>) => void;
 };
 
-const TableControl = ({ listType, toggleListType }: TableControlProps) => {
+const TableControl = ({
+  listType,
+  toggleListType,
+  filterNetworks,
+  toggleNetworkFilter,
+  filterDelegate,
+  toggleDelegateFilter,
+}: TableControlProps) => {
   return (
     <Layout>
       <StyledInput
@@ -71,8 +73,18 @@ const TableControl = ({ listType, toggleListType }: TableControlProps) => {
         id="table-search"
         placeholder="Search 3 Daos"
       />
-      <FilterDropdown />
-      <Button secondary onClick={toggleListType} IconLeft={IconGrid}>
+      <FilterDropdown
+        filterNetworks={filterNetworks}
+        toggleNetworkFilter={toggleNetworkFilter}
+        filterDelegate={filterDelegate}
+        toggleDelegateFilter={toggleDelegateFilter}
+      />
+      <Button
+        secondary
+        onClick={toggleListType}
+        IconLeft={IconGrid}
+        className="list-type-toggle"
+      >
         {listType === 'table' ? 'Card View' : 'List View'}
       </Button>
     </Layout>
@@ -80,108 +92,3 @@ const TableControl = ({ listType, toggleListType }: TableControlProps) => {
 };
 
 export default TableControl;
-
-const DropdownButton = styled(Button)`
-  &.selected {
-    background-color: ${(props: { theme: Theme }) => props.theme.secondary};
-  }
-`;
-
-const FilterDropdown = () => {
-  const [filterNetworks, setFilterNetworks] = useState<Record<string, string>>(
-    {}
-  );
-  const theme = useTheme();
-
-  const toggleNetworkFilter = (event: MouseEvent<HTMLButtonElement>) => {
-    const network = event.currentTarget.value;
-    if (network && isValidNetwork(network)) {
-      filterNetworks[network]
-        ? setFilterNetworks((prevState) => {
-            delete prevState[network];
-            return { ...prevState };
-          })
-        : setFilterNetworks((prevState) => ({
-            ...prevState,
-            [network]: network,
-          }));
-    }
-  };
-
-  const networkButtons = Object.values(networkData).map(
-    (network): DropdownItem => {
-      console.log('RENDER');
-      const isActive = filterNetworks[network.chainId];
-      console.log('filterNetworks', filterNetworks);
-      console.log('network.chainId', network.chainId);
-      console.log('isActive ', isActive);
-      return {
-        type: 'clickable',
-        content: (
-          <DropdownButton
-            key={network.chainId}
-            value={network.chainId}
-            onClick={toggleNetworkFilter}
-            className={isActive ? 'selected' : ''}
-            secondary
-            fullWidth
-            leftAlign
-            IconRight={isActive ? AiOutlineCheck : undefined}
-          >
-            <div style={{ width: '100%' }}>{network.name}</div>
-          </DropdownButton>
-        ),
-      };
-    }
-  );
-
-  return (
-    <Dropdown
-      width="25rem"
-      spacing=".6rem"
-      bg={theme.button.secondary.bg}
-      trigger={
-        <Button secondary IconLeft={IconFilter}>
-          Filters
-        </Button>
-      }
-      items={[
-        { type: 'label', content: <ParSm>Networks</ParSm> },
-        ...networkButtons,
-        //   type: 'clickable',
-        //   content: (
-        //     <DropdownButton
-        //       secondary
-        //       fullWidth
-        //       leftAlign
-        //       value="0x5"
-        //       onClick={toggleNetworkFilter}
-        //     >
-        //       Goerli
-        //     </DropdownButton>
-        //   ),
-        // },
-        {
-          type: 'label',
-          content: <ParSm>Delegation</ParSm>,
-        },
-        {
-          type: 'clickable',
-          content: (
-            <DropdownButton secondary fullWidth leftAlign>
-              I am a Delegate
-            </DropdownButton>
-          ),
-        },
-        {
-          type: 'clickable',
-          content: (
-            <DropdownButton secondary fullWidth leftAlign>
-              I have a Delegate
-            </DropdownButton>
-          ),
-        },
-      ]}
-    />
-  );
-};

54b34c23ed5227a1d1a0ea8da3ed9dd4f60d7916
Spencer Graham <code@spengrah.xyz>
2022-07-05 13:13:02 -0500

    Spencer's Summon tooltip suggestions
    - Summon portion of #365 
    - #368

--- apps/summon-app/src/utils/content.ts
+++ apps/summon-app/src/utils/content.ts
@@ -2,51 +2,51 @@ export const FORM_COPY = {
   TOKENS: {
     title: 'Stake Tokens',
     description:
-      'The Stake tokens represent voting and economic stake within the DAO.',
+      'The Stake tokens represent both voting weight and economic stake within the DAO.',
   },
   TIMING: {
     title: 'Proposal Timing',
     description:
-      'Define your timing for Voting and Grace periods. You can update these settings through a proposal.',
+      'Define the timing for Voting and Grace periods. After summoning, the DAO can update these settings through a proposal.',
   },
   ADVANCED: {
     title: 'Advanced Governance',
-    description: 'Modify some advanced governance features.',
+    description: 'Configure some advanced governance settings.',
   },
   SHAMANS: {
     title: 'Starting Shamans',
     description:
-      'Shamans are very powerful as they can have administrative control over voting and non-voting stakes. Be very careful adding shamans.',
+      'Shamans are very powerful as they can have administrative control over key components of the DAO, including voting and non-voting stake. Be very careful adding shamans.',
   },
   MEMBERS: {
     title: 'Starting Members',
     description:
-      'You must have at least one member to start. Add other starting members as desired. You can always add more members later through a proposal or a shaman.',
+      'You must have at least one member to start. Add other starting members as desired. You can always add more members later through a proposal.',
   },
 };
 export const INFO_COPY = {
   VOTING_STK:
-    "Voting stake tokens are created when the DAO is summoned. What does your community want to name the DAO's token?",
+    "The voting stake token name is set when the DAO is summoned. What does your community want to name the DAO's token?",
   TOKEN_SYMBOL:
-    'This is the common shorthand referring to stake among DAO members.  What abbreviated symbol would your DAO like to use here?',
+    'This is the common shorthand your DAO will use to refer to voting stake among DAO members. What abbreviated symbol would your DAO like to use here?',
   STAKE_TRANSFER:
-    'Allow or disallow the voting stake tokens to be transferable between addresses.',
+    'Should DAO members be allowed to transfer their voting stake to other accounts? After summoning, the DAO can change this with a proposal.',
   NV_STAKE_TRANSFER:
-    'Allow or disallow the non-voting stake tokens to be transferable between addresses',
+    'Should DAO members be allowed to transfer their non-voting stake to other accounts. After summoning, the DAO can change this with a proposal.',
   VOTING_PERIOD:
-    'The length of time a proposal will remain available for voting stake members to submit their vote',
+    'The length of time a proposal will remain open for voting stake members to submit votes.',
   GRACE_PERIOD:
-    'The time period between a proposal’s approval and its execution.  This provides time for members to ragequit their shares, before the approved proposal is executed.',
+    'The time period between a proposal’s approval and its execution. This provides time for members to ragequit their shares, before the approved proposal is executed.',
   QUORUM:
-    'The minimum percentage of DAO members needed to approve a proposal for it to execute.  This percentage can greatly affect DAO operations',
+    'The minimum percentage of DAO members (voting stake) needed to approve a proposal for it to be executed. This percentage can greatly affect DAO operations, and it the DAO can change it with a proposal.',
   MIN_RETENTION:
-    'Used as a safety measure, this is the minimum percentage of DAO members needed to ragequit for a proposal to automatically fail',
+    'If less than this percentage of DAO members (voting and non-voting stake) remain in the DAO after the Grace period for a given proposal, that proposal will not be executed.',
   SPONSOR_THRESHOLD:
-    'The minimum amount of voting stake that a member needs to have their proposal automatically sponsored upon creation.',
+    'The minimum number of voting stake tokens that a member needs to sponsor a proposal.',
   NEW_OFFERING:
-    'Intended to protect against spam issues, this is the fee required to submit a DAO proposal.',
+    'Intended to protect against spam issues, this is the fee required to submit a DAO proposal. Any fees go into the DAO treasury.',
   SHAMAN:
-    'Input shaman list with contract address and permission level per row using spaces. ex: \n0x00000000000000 2 \n0x00000000000000 1',
+    'Input shaman list with contract address and permission level per row using spaces. ex: \n0x00000000000000 2 \n0x00000000000000 1.',
   MEMBERS:
-    'Input member list with member address, voting stake amount, and non-voting stake amount per row. ex: \n 0x00000000000000 20 10 \n 0x00000000000000 10 20',
+    'Input member list with member address, voting stake amount, and non-voting stake amount per row. ex: \n 0x00000000000000 20 10 \n 0x00000000000000 10 20.',
 };

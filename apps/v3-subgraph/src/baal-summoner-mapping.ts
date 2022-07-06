a613039b803ac0d65d655eb66f0ae1e67cb7db6b
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-07-06 12:21:15 -0600

    Feature/alpha baal update (#376)
    * new abis
    
    * updates summoner tx logic and contract addresses
    
    * fixes transaction entity creation location;
    
    * fixes some test

--- apps/v3-subgraph/src/baal-summoner-mapping.ts
+++ apps/v3-subgraph/src/baal-summoner-mapping.ts
@@ -5,9 +5,7 @@ import {
   SharesTemplate,
 } from '../generated/templates';
 import { Dao, TokenLookup } from '../generated/schema';
-import { addTransaction } from './util/transactions';
 import { constants } from './util/constants';
-import { log } from '@graphprotocol/graph-ts';
 
 export function handleSummonBaal(event: SummonBaal): void {
   BaalTemplate.create(event.params.baal);
@@ -51,6 +49,4 @@ export function handleSummonBaal(event: SummonBaal): void {
   lootTokenLookup.dao = event.params.baal;
 
   lootTokenLookup.save();
-
-  addTransaction(event.block, event.transaction, event.params.baal);
 }

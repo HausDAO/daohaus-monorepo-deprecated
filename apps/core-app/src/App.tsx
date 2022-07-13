55f910729c99367e81a37251e5559d4479c3588c
jordan <jordan.lesich@gmail.com>
2022-07-13 15:07:10 -0700

    switches to connect class padding for mobile

--- apps/core-app/src/App.tsx
+++ apps/core-app/src/App.tsx
@@ -1,11 +1,16 @@
 import { DaoHausNav } from '@daohaus/daohaus-connect-feature';
-import { H1 } from '@daohaus/ui';
+import { H1, widthQuery } from '@daohaus/ui';
 import styled from 'styled-components';
 
 const Layout = styled.div`
   width: 100%;
   .connect {
     padding: 2.6rem 3rem;
+  }
+  @media ${widthQuery.sm} {
+    .connect {
+      padding: 3rem 2rem;
+    }
   }
 `;
 

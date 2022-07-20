cffe6be3afc3af149708ecb31730beef6df5ba47
jordan <jordan.lesich@gmail.com>
2022-07-19 20:44:27 -0700

    scaffold formBuilder library

--- apps/core-app/src/App.tsx
+++ apps/core-app/src/App.tsx
@@ -1,58 +1,40 @@
-import { DaoHausNav } from '@daohaus/daohaus-connect-feature';
-import {
-  BiColumnLayout,
-  Card,
-  MainLayout,
-  SubNav,
-  widthQuery,
-} from '@daohaus/ui';
-import { OuterLayout } from '@daohaus/ui';
-import styled from 'styled-components';
+import { HausLayout } from '@daohaus/daohaus-connect-feature';
+import { FormBuilder } from '@daohaus/haus-form-builder';
 
-const LeftCard = styled(Card)`
-  width: 100%;
-  min-width: 54rem;
-  max-width: 64rem;
-  height: 47rem;
-  @media ${widthQuery.md} {
-    max-width: 100%;
-    min-width: 0;
-  }
-`;
+// const LeftCard = styled(Card)`
+//   width: 100%;
+//   min-width: 54rem;
+//   max-width: 64rem;
+//   height: 47rem;
+//   @media ${widthQuery.md} {
+//     max-width: 100%;
+//     min-width: 0;
+//   }
+// `;
 
-const RightCard = styled(Card)`
-  width: 100%;
-  min-width: 38rem;
-  max-width: 45rem;
-  height: 77rem;
-  @media ${widthQuery.md} {
-    max-width: 100%;
-    min-width: 0;
-  }
-`;
+// const RightCard = styled(Card)`
+//   width: 100%;
+//   min-width: 38rem;
+//   max-width: 45rem;
+//   height: 77rem;
+//   @media ${widthQuery.md} {
+//     max-width: 100%;
+//     min-width: 0;
+//   }
+// `;
 
 export function App() {
   return (
-    <OuterLayout>
-      <DaoHausNav />
-      <SubNav
-        navLinks={[
-          { label: 'Home', href: '/home' },
-          { label: 'Proposals', href: '/proposals' },
-          { label: 'Vaults', href: '/vaults' },
-          { label: 'Members', href: '/members' },
-        ]}
-        dropdownLinks={[{ label: 'Settings', href: '/settings' }]}
-      />
-      <MainLayout>
-        <BiColumnLayout
-          subtitle="Disperse Proposal"
-          title="Disperse Reimbursements for April ‘22 Event"
-          left={<LeftCard>Left</LeftCard>}
-          right={<RightCard>Right</RightCard>}
-        />
-      </MainLayout>
-    </OuterLayout>
+    <HausLayout
+      navLinks={[
+        { label: 'Home', href: '/home' },
+        { label: 'Proposals', href: '/proposals' },
+        { label: 'Vaults', href: '/vaults' },
+        { label: 'Members', href: '/members' },
+      ]}
+    >
+      <FormBuilder />
+    </HausLayout>
   );
 }
 
---
+++ libs/form-builder/src/formBuilder.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FormBuilder = ({ test }: { test: string }) => {
+  return <div>{test}</div>;
+};

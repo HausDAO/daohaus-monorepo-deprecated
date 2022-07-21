23f734b105db5d3edd8322a41e7bad936311df9e
Rowdy <mroddy5280@gmail.com>
2022-07-21 15:29:34 -0600

    Fix/445 reimplement dropdown component (#489)
    * Removed container from dropdown, removed width association from container and menu content
    
    * Fixed useage of buttons and dropdowns in hub and summon
    
    * Finished up with the hub layout
    
    * Removed Fragment
    
    * Removed Item Factory, can add in object literal or switch to compose dropdowns of many types later

--- libs/ui/src/components/atoms/Dropdown/Dropdown.styles.ts
+++ libs/ui/src/components/atoms/Dropdown/Dropdown.styles.ts
@@ -1,21 +1,20 @@
 import styled from 'styled-components';
 import * as Dropdown from '@radix-ui/react-dropdown-menu';
 
-export const DropdownContainer = styled.div`
-  width: ${(props: { width: string }) => props.width};
-`;
-
 export const DropdownMenu = Dropdown.Root;
 export const DropdownMenuTrigger = styled(Dropdown.Trigger)``;
+export const DropdownMenuTriggerItem = styled(Dropdown.TriggerItem);
 
 type MenuContentType = {
   bg?: string;
   width?: string;
 };
+
 export const DropdownMenuContent = styled(Dropdown.DropdownMenuContent)`
   background-color: ${(props: MenuContentType) => props.bg};
   width: ${(props: MenuContentType) => props.width};
 `;
+
 export const DropdownMenuItem = styled(Dropdown.Item)`
   margin-bottom: ${(props: { spacing?: string }) => props.spacing};
   margin-left: ${(props: { spacing?: string }) => props.spacing};
@@ -25,4 +24,3 @@ export const DropdownMenuItem = styled(Dropdown.Item)`
     margin-top: ${(props: { spacing?: string }) => props.spacing};
   }
 `;
-export const DropdownMenuTriggerItem = styled(Dropdown.TriggerItem);

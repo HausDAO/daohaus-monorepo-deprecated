c9a9d8ea96e567912a6d18ed6d8b486a9b2d8cd1
jordan <jordan.lesich@gmail.com>
2022-07-29 09:53:24 -0700

    build metadata form

--- apps/core-app/src/pages/Home.tsx
+++ apps/core-app/src/pages/Home.tsx
@@ -1,14 +1,11 @@
 import { HausLayout } from '@daohaus/daohaus-connect-feature';
 import { FormBuilder } from '@daohaus/haus-form-builder';
-import { GovernanceProposal } from '../data/forms';
+import { MetadataConfig } from '../data/formLegos';
 
 export function Home() {
   return (
     <HausLayout navLinks={[{ label: 'NavLink', href: '/' }]}>
-      <FormBuilder
-        form={GovernanceProposal}
-        onSubmit={() => console.log('test')}
-      />
+      <FormBuilder form={MetadataConfig} onSubmit={() => console.log('test')} />
     </HausLayout>
   );
 }

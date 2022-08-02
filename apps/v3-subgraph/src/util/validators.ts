53145772375c1b9a3069b93c9b10ebf512b7ec23
skuhlmann <samkuhlmann@gmail.com>
2022-08-02 11:41:57 -0600

    node version issues and poster mapping fixes

--- apps/v3-subgraph/src/util/validators.ts
+++ apps/v3-subgraph/src/util/validators.ts
@@ -13,12 +13,16 @@ export namespace validators {
 
     log.info('validating memberId, {}', [memberId]);
 
+    // 0x24357654d64da97a55fb2f73c5b66d1927ab1e4c-member-0x83ab8e31df35aa3281d630529c6f4bf5ac7f7abf
+
     const member = Member.load(memberId);
     if (!member || member.shares == constants.BIGINT_ZERO) {
       log.info('no member or shares', []);
 
       return false;
     }
+
+    log.info('found member with shares', []);
     return true;
   }
 }

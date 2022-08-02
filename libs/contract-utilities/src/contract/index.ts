1389d09fe31191cd66df5e4c21dab602e5ecc3ee
Brian Rossetti <brossetti1@gmail.com>
2022-08-01 19:02:36 -0600

    using uppercase snake case format

--- libs/contract-utilities/src/contract/index.ts
+++ libs/contract-utilities/src/contract/index.ts
@@ -13,11 +13,11 @@ export { getContractsForChainOrThrow } from './contracts';
 export { ChainId, ContractAddresses } from './types';
 
 export {
-  GnosisMultisendABI,
-  BaalABI,
-  BaalSummonerABI,
-  LootABI,
-  SharesABI,
-  PosterABI,
-  TributeMinionABI,
+  GnosisMultisendABI as GNOSIS_MULTISEND_ABI,
+  BaalABI as BAAL_ABI,
+  BaalSummonerABI as BAAL_SUMMONER_ABI,
+  LootABI as LOOT_ABI,
+  SharesABI as SHARES_ABI,
+  PosterABI as POSTER_ABI,
+  TributeMinionABI as TRIBUTE_MINION_ABI,
 };

232ded1fa2faddc669eccd7041df9b60baf40e09
jordan <jordan.lesich@gmail.com>
2022-08-01 22:34:43 -0700

    add prop and type for arbitrary data

--- apps/summon-app/src/app/TXBuilder.tsx
+++ apps/summon-app/src/app/TXBuilder.tsx
@@ -3,25 +3,39 @@ import { createContext, useState, useMemo, useContext, ReactNode } from 'react';
 import { isValidNetwork } from '@daohaus/common-utilities';
 import { handleFireTx, TX, TxRecord } from '../utils/txBuilderUtils';
 
+// Truly should be any here.
+// Possible TS challenge if we feel like adding a user defined type.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ArbitraryAppState = Record<string, any>;
+
 type TxContext = {
   transactions: TxRecord;
   txAmt: number;
   fireTransaction: (tx: TX) => void;
+  appState: ArbitraryAppState;
 };
 
 export const TxBuilderContext = createContext<TxContext>({
   transactions: {},
   fireTransaction: () => undefined,
   txAmt: 0,
+  appState: {},
 });
 
 type BuilderProps = {
   chainId: string | undefined | null;
   provider: providers.Web3Provider | undefined | null;
   children: ReactNode;
+
+  appState: ArbitraryAppState;
 };
 
-export const TXBuilder = ({ chainId, provider, children }: BuilderProps) => {
+export const TXBuilder = ({
+  chainId,
+  provider,
+  appState,
+  children,
+}: BuilderProps) => {
   const [transactions, setTransactions] = useState<TxRecord>({});
   const txAmt = useMemo(() => {
     return Object.values(transactions).length;
@@ -38,7 +52,9 @@ export const TXBuilder = ({ chainId, provider, children }: BuilderProps) => {
   }
 
   return (
-    <TxBuilderContext.Provider value={{ transactions, fireTransaction, txAmt }}>
+    <TxBuilderContext.Provider
+      value={{ transactions, fireTransaction, txAmt, appState }}
+    >
       {children}
     </TxBuilderContext.Provider>
   );

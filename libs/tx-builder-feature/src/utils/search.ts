0257ca5e3b374ba3ecbcafba9cf4f0d7830484dc
jordan <jordan.lesich@gmail.com>
2022-08-07 17:49:37 -0700

    implement search

---
+++ libs/tx-builder-feature/src/utils/search.ts
@@ -0,0 +1,30 @@
+import { ArbitraryState, StringSearch } from '@daohaus/common-utilities';
+
+export const deepSearch = (
+  appState: ArbitraryState,
+  pathString: StringSearch
+) => {
+  const path = pathString.trim().split('.').filter(Boolean);
+  let state = { ...appState };
+  for (let i = 0, len = path.length; i < len; i++) {
+    state = state?.[path?.[i]];
+  }
+  return state;
+};
+
+export const searchApp = (
+  appState: ArbitraryState,
+  pathString: StringSearch,
+  shouldThrow = false
+) => {
+  const result = deepSearch(appState, pathString) || false;
+  if (!result) {
+    if (shouldThrow) {
+      console.log('**Application State**', appState);
+      throw new Error(`Could not find ${pathString}`);
+    } else {
+      return false;
+    }
+  }
+  return result;
+};

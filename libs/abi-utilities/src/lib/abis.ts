5f11b163cd3ece32ec5349eced8a677b8773f1ba
Brian Rossetti <brossetti1@gmail.com>
2022-08-11 00:00:26 -0600

    adding abi-utils

--- libs/abi-utilities/src/lib/abis.ts
+++ libs/abi-utilities/src/lib/abis.ts
@@ -1,12 +1,36 @@
-import BAAL from '../abi/baal.json';
-import BAAL_SUMMONER from '../abi/baalSummoner.json';
-import GNOSIS_MULTISEND from '../abi/gnosisMultisend.json';
-import LOOT from '../abi/loot.json';
-import POSTER from '../abi/poster.json';
-import SHARES from '../abi/shares.json';
-import TRIBUTE_MINION from '../abi/tributeMinion.json';
+import BAAL from '../abi/Baal.json';
+import BAAL_SUMMONER from '../abi/BaalSummoner.json';
+import GNOSIS_MULTISEND from '../abi/GnosisMultisend.json';
+import LOOT from '../abi/Loot.json';
+import POSTER from '../abi/Poster.json';
+import SHARES from '../abi/Shares.json';
+import TRIBUTE_MINION from '../abi/TributeMinion.json';
 
-export const LOCAL_ABI = {
+interface JsonFragmentType {
+  readonly name?: string;
+  readonly indexed?: boolean;
+  readonly type?: string;
+  readonly internalType?: string;
+  readonly components?: ReadonlyArray<JsonFragmentType>;
+}
+
+interface JsonFragment {
+  readonly name?: string;
+  readonly type?: string;
+
+  readonly anonymous?: boolean;
+
+  readonly payable?: boolean;
+  readonly constant?: boolean;
+  readonly stateMutability?: string;
+
+  readonly inputs?: ReadonlyArray<JsonFragmentType>;
+  readonly outputs?: ReadonlyArray<JsonFragmentType>;
+
+  readonly gas?: string;
+}
+
+export const LOCAL_ABI: Record<string, (JsonFragmentType | JsonFragment)[]> = {
   BAAL,
   BAAL_SUMMONER,
   GNOSIS_MULTISEND,

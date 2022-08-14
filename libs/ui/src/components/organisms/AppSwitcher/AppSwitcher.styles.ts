178555972d61c1ba27f6a3e80f0760e3146a3a17
Rowdy <mroddy5280@gmail.com>
2022-08-13 21:47:30 -0600

    Added style for chevron

--- libs/ui/src/components/organisms/AppSwitcher/AppSwitcher.styles.ts
+++ libs/ui/src/components/organisms/AppSwitcher/AppSwitcher.styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import { Theme } from '../../../types/theming';
 import { DropdownButton } from '../../molecules';
 
 export const AppSwitcherTrigger = styled(DropdownButton)`
@@ -7,6 +8,12 @@ export const AppSwitcherTrigger = styled(DropdownButton)`
   padding: 0 16px;
 
   svg.icon-right {
+    color: ${({ theme }: { theme: Theme }) => theme.primary};
     margin-left: auto;
+  }
+
+  svg.icon-left {
+    // TODO Need to know if colors are already applied to App SVGs
+    color: ${({ theme }: { theme: Theme }) => theme.primary};
   }
 `;

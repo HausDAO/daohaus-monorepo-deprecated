0fa2446b5052fdc8706913a073167cc22da3fe73
jordan <jordan.lesich@gmail.com>
2022-08-13 20:40:29 -0700

    fix error modals in formBuilder

--- libs/common-utilities/src/utils/typeguards.ts
+++ libs/common-utilities/src/utils/typeguards.ts
@@ -12,7 +12,6 @@ export const isBoolean = (item: unknown): item is boolean =>
   typeof item === 'boolean';
 export const isNumberish = (item: unknown): item is string | number =>
   isNumber(item) || isNumberString(item);
-
 // general 'is' guards that help us verify shapes of data
 
 export const isArgType = (item: unknown): item is ArgType => {

e319dc359be8ed3711d391cd27152fcd5ebfac0a
jordan <jordan.lesich@gmail.com>
2022-08-16 16:44:23 -0700

    clean

--- apps/core-app/src/components/customFields/tributeInput.tsx
+++ apps/core-app/src/components/customFields/tributeInput.tsx
@@ -1,30 +1,20 @@
 import { DaoWithTokenData } from '@daohaus/dao-data';
-import { useTxBuilder } from '@daohaus/tx-builder-feature';
 import { Buildable, InputSelectProps, WrappedInputSelect } from '@daohaus/ui';
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { useDao } from '../../contexts/DaoContext';
 
 type TributeInputProps = Omit<InputSelectProps, 'options'>;
 
 const tokenArrayFromDaoContext = (dao: DaoWithTokenData) => {
-  console.log(dao);
-  // const nonVotingToken = {
-  //   address: dao.lootAddress,
-  //   name:
-  // };
-  // const votingToken =
+  return [];
 };
 
 export const TributeInput = (props: Buildable<TributeInputProps>) => {
-  const { fireTransaction } = useTxBuilder();
-
   const { dao } = useDao();
 
   const daoTokens = useMemo(() => {
-    if (dao?.tokenBalances) {
-      return tokenArrayFromDaoContext(dao);
-    }
+    if (dao) return tokenArrayFromDaoContext(dao);
   }, [dao]);
 
-  return <WrappedInputSelect {...props} options={[]} />;
+  return <WrappedInputSelect {...props} options={daoTokens || []} />;
 };

7f47163a019820df6293dfad52d9de27188becbe
Brian Rossetti <brossetti1@gmail.com>
2022-08-15 20:38:35 -0600

    refacotring out contract abis

--- libs/abi-utilities/src/lib/abis.ts
+++ libs/abi-utilities/src/lib/abis.ts
@@ -1,52 +1,33 @@
+import { ABI } from '@daohaus/common-utilities';
 import BAAL from '../abi/baal.json';
 import BAAL_SUMMONER from '../abi/baalSummoner.json';
 import GNOSIS_MULTISEND from '../abi/gnosisMultisend.json';
+import GNOSIS_PROXY from '../abi/gnosisProxy.json';
 import LOOT from '../abi/loot.json';
 import POSTER from '../abi/poster.json';
 import SHARES from '../abi/shares.json';
+import SUPERFLUID_PROXY from '../abi/superfluidProxy.json';
 import TRIBUTE_MINION from '../abi/tributeMinion.json';
-interface JsonFragmentType {
-  readonly name?: string;
-  readonly indexed?: boolean;
-  readonly type?: string;
-  readonly internalType?: string;
-  readonly components?: ReadonlyArray<JsonFragmentType>;
-}
-
-interface JsonFragment {
-  readonly name?: string;
-  readonly type?: string;
-
-  readonly anonymous?: boolean;
-
-  readonly payable?: boolean;
-  readonly constant?: boolean;
-  readonly stateMutability?: string;
-
-  readonly inputs?: ReadonlyArray<JsonFragmentType>;
-  readonly outputs?: ReadonlyArray<JsonFragmentType>;
-
-  readonly gas?: string;
-}
 
 type ContractABIKey =
   | 'BAAL'
   | 'BAAL_SUMMONER'
   | 'GNOSIS_MULTISEND'
+  | 'GNOSIS_PROXY'
   | 'LOOT'
   | 'POSTER'
   | 'SHARES'
+  | 'SUPERFLUID_PROXY'
   | 'TRIBUTE_MINION';
 
-export const LOCAL_ABI: Record<
-  ContractABIKey,
-  (JsonFragmentType | JsonFragment)[]
-> = {
+export const LOCAL_ABI: Record<ContractABIKey, ABI> = {
   BAAL,
   BAAL_SUMMONER,
   GNOSIS_MULTISEND,
+  GNOSIS_PROXY,
   LOOT,
   POSTER,
   SHARES,
+  SUPERFLUID_PROXY,
   TRIBUTE_MINION,
 };

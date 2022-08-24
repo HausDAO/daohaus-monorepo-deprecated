478ddbc2a0d1ad0592cafa2dd494ef8cef9105c8
skuhlmann <samkuhlmann@gmail.com>
2022-08-24 14:33:14 -0600

    adds styling and hover

--- apps/core-app/src/components/NewProposalList.tsx
+++ apps/core-app/src/components/NewProposalList.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { RiArrowRightSLine } from 'react-icons/ri';
 import styled from 'styled-components';
-import { Bold, DataSm, Link, ParMd, Theme } from '@daohaus/ui';
+import { Bold, border, DataSm, Link, ParMd, Theme } from '@daohaus/ui';
 
 import { CustomFormLego } from '../legos/config';
 
@@ -9,15 +9,31 @@ const ListContainer = styled.div`
   margin-top: 5rem;
 `;
 
-const ListItemContainer = styled(Link)`
-  display: flex;
-  justify-content: space-between;
+const ListItemContainer = styled.div`
+  width: 100%;
+  padding: 1rem 0;
   border-top: 1px ${({ theme }: { theme: Theme }) => theme.card.hoverBorder}
     solid;
-  padding: 3rem 0;
+`;
+
+const ListItemLink = styled(Link)`
+  width: 100%;
   color: unset;
   :hover {
     text-decoration: none;
+  }
+`;
+
+const ListItemHoverContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 1rem;
+  border-radius: ${border.radius};
+
+  :hover {
+    background: 1px ${({ theme }: { theme: Theme }) => theme.card.hoverBg};
   }
 `;
 
@@ -26,6 +42,7 @@ const ListItem = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   word-wrap: break-word;
+  max-width: 39rem;
 `;
 
 const StyledIcon = styled(RiArrowRightSLine)`
@@ -42,16 +59,20 @@ export const NewProposalList = ({ proposalLegos }: NewProposalListProps) => {
   return (
     <ListContainer>
       {proposalLegos.map((proposalLego: CustomFormLego) => (
-        <ListItemContainer
-          href={`/molochv3/${daochain}/${daoid}/new-proposal?formLego=${proposalLego.id}`}
-        >
-          <ListItem>
-            <ParMd>
-              <Bold>{proposalLego.title}</Bold>
-            </ParMd>
-            <DataSm>{proposalLego.description}</DataSm>
-          </ListItem>
-          <StyledIcon />
+        <ListItemContainer>
+          <ListItemLink
+            href={`/molochv3/${daochain}/${daoid}/new-proposal?formLego=${proposalLego.id}`}
+          >
+            <ListItemHoverContainer>
+              <ListItem>
+                <ParMd>
+                  <Bold>{proposalLego.title}</Bold>
+                </ParMd>
+                <DataSm>{proposalLego.description}</DataSm>
+              </ListItem>
+              <StyledIcon />
+            </ListItemHoverContainer>
+          </ListItemLink>
         </ListItemContainer>
       ))}
     </ListContainer>

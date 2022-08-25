3070d91cf75b67e18c51a6a3ee6693e32244472e
Brian Rossetti <brossetti1@gmail.com>
2022-08-25 09:35:03 -0600

    adddding settings update form (#709)
    * adddding settings update form
    
    * cleaning up
    
    * adding default values to form
    
    * updating dao load
    
    Co-authored-by: Sam Kuhlmann <samkuhlmann@gmail.com>

--- apps/core-app/src/legos/config.tsx
+++ apps/core-app/src/legos/config.tsx
@@ -5,6 +5,7 @@ import { ToWeiInput } from '../components/customFields/toWeiInput';
 import { NestedArray } from '../components/customFields/nestedArrayInput';
 import { RequestNativeToken } from '../components/customFields/RequestNativeToken';
 import { RequestERC20 } from '../components/customFields/RequestERC20';
+import { TagsInput } from '../components/customFields/tagsInput';
 
 export const CustomFields = {
   ...CoreFieldLookup,
@@ -13,6 +14,7 @@ export const CustomFields = {
   nestedArray: NestedArray,
   requestNativeToken: RequestNativeToken,
   requestERC20: RequestERC20,
+  tagsInput: TagsInput,
 };
 
 export type CustomFieldLego = FieldLegoBase<typeof CustomFields>;

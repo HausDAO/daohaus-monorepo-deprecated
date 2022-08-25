43614f90cb688beecc63f33f3f0dd8f527ebf56f
jordan <jordan.lesich@gmail.com>
2022-08-24 23:39:34 -0700

    clean logs

--- libs/ui/src/components/molecules/WrappedInput/WrappedInput.tsx
+++ libs/ui/src/components/molecules/WrappedInput/WrappedInput.tsx
@@ -4,10 +4,6 @@ import { Input } from '../../atoms';
 import { FieldWrapper } from '../FieldWrapper/FieldWrapper';
 
 export const WrappedInput = ({ id, rules, ...props }: Buildable<Field>) => {
-  if (id === 'shamanAddress') {
-    console.log('id', id);
-    console.log('props.disabled', props.disabled);
-  }
   const { register } = useFormContext();
   return (
     <FieldWrapper {...props} id={id} rules={rules}>

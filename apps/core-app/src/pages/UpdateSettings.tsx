4038c178c838da9b57ddd4687e6812b541356342
Brian Rossetti <brossetti1@gmail.com>
2022-08-24 18:05:43 -0600

    adding default values to form

--- apps/core-app/src/pages/UpdateSettings.tsx
+++ apps/core-app/src/pages/UpdateSettings.tsx
@@ -1,10 +1,30 @@
 import { FormBuilder } from '@daohaus/haus-form-builder';
+import { useDao } from '../contexts/DaoContext';
 import { CustomFields } from '../legos/config';
 import { FORM } from '../legos/form';
 
 export function UpdateSettings() {
+  const { dao } = useDao();
+  const links = dao?.links && JSON.parse(dao?.links);
+  const defaultFields = {
+    name: dao?.name,
+    icon: dao?.avatarImg,
+    tags: dao?.tags?.join(', '),
+    description: dao?.description,
+    long_description: dao?.longDescription,
+    ...links,
+  };
+
+  if (!dao) {
+    return null;
+  }
+
   return (
-    <FormBuilder form={FORM.METADATA_SETTINGS} customFields={CustomFields} />
+    <FormBuilder
+      defaultValues={defaultFields}
+      form={{ ...FORM.METADATA_SETTINGS, log: true }}
+      customFields={CustomFields}
+    />
   );
 }
 

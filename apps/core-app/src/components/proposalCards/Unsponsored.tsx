8b5541f9dd8a1db763799d34b052284465d043f3
jordan <jordan.lesich@gmail.com>
2022-08-30 19:16:45 -0700

    enable tooltip if shares

--- apps/core-app/src/components/proposalCards/Unsponsored.tsx
+++ apps/core-app/src/components/proposalCards/Unsponsored.tsx
@@ -1,10 +1,11 @@
+import { isNumberish } from '@daohaus/common-utilities';
 import { ITransformedProposal } from '@daohaus/dao-data';
 import { useTxBuilder } from '@daohaus/tx-builder-feature';
 import { Button, Italic, ParSm } from '@daohaus/ui';
 import React, { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { useTheme } from 'styled-components';
-import { useDao } from '../../contexts/DaoContext';
+import { useConnectedMembership, useDao } from '../../contexts/DaoContext';
 import { PROP_CARD_HELP } from '../../data/copy';
 import { ActionTemplate, DummyBar } from './ActionPrimitives';
 import { GatedButton } from './GatedButton';
@@ -16,14 +17,23 @@ export const Unsponsored = ({
 }) => {
   const { daochain } = useParams();
   const { fireTransaction } = useTxBuilder();
+  const { connectedMembership } = useConnectedMembership();
+
   const { dao } = useDao();
   const theme = useTheme();
-  const sponsorProposal = () => {
-    console.log('sponsorProposal');
-  };
-  // const SponsorButton = useMemo(() => {
-  //   // if()
-  // }, [proposal, dao]);
+
+  const hasShares = useMemo(() => {
+    if (
+      isNumberish(connectedMembership?.shares) &&
+      isNumberish(dao?.sponsorThreshold)
+    ) {
+      return (
+        Number(connectedMembership?.shares) > Number(dao?.sponsorThreshold)
+      );
+    }
+    return false;
+  }, [dao, connectedMembership]);
+
   return (
     <ActionTemplate
       statusDisplay="Needs A Sponsor"
@@ -32,7 +42,7 @@ export const Unsponsored = ({
           <DummyBar />
           <GatedButton
             sm
-            rules={[false]}
+            rules={[hasShares]}
             tooltipContent={PROP_CARD_HELP.UNSPONSORED}
           >
             Sponsor Proposal

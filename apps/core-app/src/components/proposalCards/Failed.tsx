d07a355666f0429ef2c93e5d1805df0648824336
jordan <jordan.lesich@gmail.com>
2022-09-01 21:41:13 -0700

    failed

--- apps/core-app/src/components/proposalCards/Failed.tsx
+++ apps/core-app/src/components/proposalCards/Failed.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import { roundedPercentage } from '@daohaus/common-utilities';
+import { ITransformedProposal } from '@daohaus/dao-data';
+import { ActionTemplate, DummyBar, Verdict } from './ActionPrimitives';
 
-export const Failed = () => {
-  return <div>Failed</div>;
+export const Failed = ({ proposal }: { proposal: ITransformedProposal }) => {
+  const percentNo = roundedPercentage(
+    Number(proposal.noBalance),
+    Number(proposal.dao.totalShares)
+  );
+
+  return (
+    <ActionTemplate
+      statusDisplay="Proposal Failed"
+      main={
+        <>
+          <DummyBar />
+          <Verdict passed={false} appendText={` - ${percentNo}% No`} />{' '}
+        </>
+      }
+    />
+  );
 };

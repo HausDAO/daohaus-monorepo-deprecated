d5084b02431492ec8b5dfb6fc7ecca55bfd41770
Rowdy <mroddy5280@gmail.com>
2022-09-02 14:41:42 -0600

    Added Banner to two Apps, Fixed styles for mobile

--- apps/core-app/src/Routes.tsx
+++ apps/core-app/src/Routes.tsx
@@ -14,31 +14,35 @@ import Settings from './pages/Settings';
 import NewProposal from './pages/NewProposal';
 import UpdateSettings from './pages/UpdateSettings';
 import ProposalDetails from './pages/ProposalDetails';
+import { Banner } from '@daohaus/ui';
 
 const Routes = () => {
   return (
-    <RoutesDom>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="molochv3/:daochain/:daoid"
-        element={
-          <DaoContextProvider>
-            <Dao />
-          </DaoContextProvider>
-        }
-      >
-        <Route index element={<DaoOverview />} />
-        <Route path="formtest" element={<FormTest />} />
-        <Route path="proposals" element={<Proposals />} />
-        <Route path="new-proposal" element={<NewProposal />} />
-        <Route path="proposals/:proposalId" element={<ProposalDetails />} />
-        <Route path="vaults" element={<Vaults />} />
-        <Route path="members" element={<Members />} />
-        <Route path="members/:memberAddress" element={<Member />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="settings/update" element={<UpdateSettings />} />
-      </Route>
-    </RoutesDom>
+    <>
+      <Banner />
+      <RoutesDom>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="molochv3/:daochain/:daoid"
+          element={
+            <DaoContextProvider>
+              <Dao />
+            </DaoContextProvider>
+          }
+        >
+          <Route index element={<DaoOverview />} />
+          <Route path="formtest" element={<FormTest />} />
+          <Route path="proposals" element={<Proposals />} />
+          <Route path="new-proposal" element={<NewProposal />} />
+          <Route path="proposals/:proposalId" element={<ProposalDetails />} />
+          <Route path="vaults" element={<Vaults />} />
+          <Route path="members" element={<Members />} />
+          <Route path="members/:memberAddress" element={<Member />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="settings/update" element={<UpdateSettings />} />
+        </Route>
+      </RoutesDom>
+    </>
   );
 };
 

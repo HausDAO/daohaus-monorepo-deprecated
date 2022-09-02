0364029c94a26e91de0bba8b4574794dfb5262d3
jordan <jordan.lesich@gmail.com>
2022-09-02 11:25:08 -0700

    fix token labels

--- apps/core-app/src/legos/form.ts
+++ apps/core-app/src/legos/form.ts
@@ -310,7 +310,7 @@ export const PROPOSAL_FORMS: Record<string, CustomFormLego> = {
                 left: {
                   id: 'vStake',
                   type: 'switch',
-                  label: 'Transferable',
+                  label: 'Voting Token Transferable',
                   info: SUMMON_COPY.STAKE_TRANSFER,
                   switches: [
                     {
@@ -325,7 +325,7 @@ export const PROPOSAL_FORMS: Record<string, CustomFormLego> = {
                 right: {
                   id: 'nvStake',
                   type: 'switch',
-                  label: 'Transferable',
+                  label: 'Non-Voting Token Transferable',
                   info: SUMMON_COPY.NV_STAKE_TRANSFER,
                   switches: [
                     {

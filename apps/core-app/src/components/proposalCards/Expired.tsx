a0e78dcbd703ca49bda5c05dea8229e6e4f25d0c
jordan <jordan.lesich@gmail.com>
2022-09-01 21:36:06 -0700

    Cancelled

--- apps/core-app/src/components/proposalCards/Expired.tsx
+++ apps/core-app/src/components/proposalCards/Expired.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { ITransformedProposal } from '@daohaus/dao-data';
+import { ActionTemplate, DummyBar, Verdict } from './ActionPrimitives';
 
-export const Expired = () => {
-  return <div>Expired</div>;
+export const Cancelled = (_props: { proposal: ITransformedProposal }) => {
+  return (
+    <ActionTemplate
+      statusDisplay="Proposal expired"
+      main={
+        <>
+          <DummyBar />
+          <Verdict passed={false} />{' '}
+        </>
+      }
+    />
+  );
 };

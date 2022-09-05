ef393fe8aa5fff3ab96d67fe933c900f9a21e08b
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-09-05 13:18:32 -0600

    Update DaoContext.tsx

--- libs/dao-context/src/DaoContext.tsx
+++ libs/dao-context/src/DaoContext.tsx
@@ -184,7 +184,6 @@ export const DaoContextProvider = ({
 }: DaoContextProviderProps) => {
   const [dao, setDao] = useState<DaoWithTokenDataQuery['dao'] | undefined>();
   const [isDaoLoading, setDaoLoading] = useState(false);
-  console.log('dao', dao);
   const [connectedMembership, setConnectedMembership] = useState<
     FindMemberQuery['member'] | undefined
   >();

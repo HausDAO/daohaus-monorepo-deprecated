f79d31e3cb821fe51ced68805aea24acca1372f2
skuhlmann <samkuhlmann@gmail.com>
2022-09-28 15:10:56 -0600

    tx and form builder testing

--- libs/form-builder/vite.config.ts
+++ libs/form-builder/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-// https://vitejs.dev/config/
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths({
@@ -10,11 +10,13 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
       output: {
         format: 'umd',
         inlineDynamicImports: true,
       },
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react-hook-form'],
     },
   },
 });
